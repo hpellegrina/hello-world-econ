a388911288f8b7df0b3459021ba5a0e8f77be62b
andres <andres.fajardo@nyu.edu>
2018-05-25 01:13:33 -0500

    Python script to create symbolic links

---
+++ input_slinks.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+# Arguments are paths to the project and dropbox data folders
+p_data = sys.argv[1] + "/data"
+d_data = sys.argv[2] + "/data"
+
+# Define function
+def symlink_force(target, link_name):
+    try:
+        os.symlink(target, link_name)
+    except FileExistsError:
+        os.remove(link_name)
+        os.symlink(target, link_name)
+
+# Make list of current sources
+sources = os.listdir(p_data)
+
+# Loop over elements in list
+for source in sources:
+    if not source == 'finaldata':
+        # Set input path
+        input = p_data + '/' + source + '/input'
+        # Call function
+        symlink_force(d_data + '/' + source + '/input', input)
